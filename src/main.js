d467ea0aa21677c60efebdc05b958afbb714c9ae
BreezeShane <346869441@qq.com>
2022-06-22 21:48:45 +0800

    Imported Element UI Plus for Vue3.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,22 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import router from './router'
 import eventBus from 'vue3-eventbus'
 
-createApp(App)
-    .use(router, eventBus)
+import ElementUI from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import {
+    components,
+    plugins
+} from './utils/elementPlus.js'
+
+const app = createApp(App)
+components.forEach(component => {
+    app.component(component.name, component)
+})
+
+plugins.forEach(plugin => {
+    app.use(plugin)
+})
+app.use(router, eventBus, ElementUI)
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app');
