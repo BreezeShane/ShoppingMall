6adcf7b5d84e410832dc6a38f17c58234efbb83e
BreezeShane <346869441@qq.com>
2022-06-22 21:18:53 +0800

    Finished the order list page.

---
+++ src/components/OrderItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <li>
+        <label>购买时间：</label><span :id="atom.id + '-date'">{{ atom.orderTime }}</span><br>
+        <label>商品名称：</label><div :id="atom.id + '-warename'">
+            <p class="orderWareName" v-for="details in atom.orderDetail" :key="details.OrderDetailid">
+                {{ details.goodsName }}
+                <span>购买数量：{{ details.nums }}</span>
+            </p>
+        </div>
+    </li>
+</template>
+
+
+
+<script>
+// import $ from 'jquery'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+// import axios from 'axios'
+
+// library.add(faCartPlus)
+
+export default {
+    name: "OrderItem",
+    props: ['atom'],
+}
+</script>
+<style lang="stylus">
+
+</style>
--- src/views/OrderFormPage.vue
+++ src/views/OrderFormPage.vue
@@ -1,13 +1,38 @@
 <template>
-    <div>
-        这是订单页面！
-    </div>
+    <router-view>
+        <div id="orderContainer">
+            <ul>
+                <order-item v-for="atom in orderList" :key="atom.id" :id="atom.id" :atom="atom"></order-item>
+            </ul>
+        </div>
+    </router-view>
 </template>
 <script>
+import axios from 'axios'
+import OrderItem from "../components/OrderItem.vue"
+
 export default {
-    
+    components: {
+        OrderItem,
+    },
+    data() {
+        let userID = sessionStorage.getItem("userID");
+        axios.get("/api/order/listByUser", {
+            params: {
+                "userId": userID,
+            }
+        }).then((res) => {
+            this.orderList.push(...res.data);
+            return res.data;
+        })
+        return {
+            orderList: []
+        }
+    },
 }
 </script>
 <style lang="stylus">
-    
+#orderContainer
+    position absolute
+    top 13%
 </style>
