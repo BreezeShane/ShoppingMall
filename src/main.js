eb9b42f4299aa836a0c703442a72be807aa7a941
BreezeShane <346869441@qq.com>
2022-06-23 11:20:27 +0800

    Removed unuseful comments, files and folders.

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import router from './router'
 import eventBus from 'vue3-eventbus'
 
-import { createVuestic } from 'vuestic-ui' // <-
-import 'vuestic-ui/dist/vuestic-ui.css' // <-
+import { createVuestic } from 'vuestic-ui'
+import 'vuestic-ui/dist/vuestic-ui.css'
 
 createApp(App).use(router, eventBus).use(createVuestic())
     .component('font-awesome-icon', FontAwesomeIcon)
